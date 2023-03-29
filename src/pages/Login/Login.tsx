5f9eb5ad078620331f9b9671f74bd590452293c3
De Nguyen <de.nguyen@vexere.com>
2023-03-30 02:15:01 +0700

    + Base component

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Login() {
+  return (
+    <div className='bg-orange'>
+      <div className='container'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form
+              className='rounded bg-white p-10 shadow-sm'
+              // onSubmit={onSubmit}
+              noValidate
+            >
+              <div className='text-2xl'>Đăng kí</div>
+              <input type="email" name='email' className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm' placeholder='Email'/>
+              <div className='mt-3'>
+                {/* <Button
+                  type='submit'
+                  className='flex  w-full items-center justify-center bg-red-500 py-4 px-2 text-sm uppercase text-white hover:bg-red-600'
+                  isLoading={loginMutation.isLoading}
+                  disabled={loginMutation.isLoading}
+                >
+                  Đăng nhập
+                </Button> */}
+              </div>
+              <div className='mt-8 flex items-center justify-center'>
+                <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
+                <Link className='ml-1 text-red-400' to='/register'>
+                  Đăng ký
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+interface Props {
+  children?: React.ReactNode
+}
+export default function Register({ children }: Props) {
+  return <div>Register {children}</div>
+}
