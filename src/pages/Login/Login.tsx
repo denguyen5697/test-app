db23246f67fe0725ec308f155388dcc8e5586029
De Nguyen <de.nguyen@vexere.com>
2023-05-01 17:22:04 +0700

    + Update validate register form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,28 +1,50 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log({ data })
+  })
+
   return (
     <div className='bg-orange'>
-      <div className='container'>
+      <div className='mx-auto max-w-7xl px-4'>
         <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form
-              className='rounded bg-white p-10 shadow-sm'
-              // onSubmit={onSubmit}
-              noValidate
-            >
-              <div className='text-2xl'>Đăng kí</div>
-              <input type="email" name='email' className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm' placeholder='Email'/>
+            <form onSubmit={onSubmit} className='rounded bg-white p-10 shadow-sm'>
+              <div className='text-2xl'>Đăng nhập</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+                  placeholder='Email'
+                  {...register('email')}
+                />
+                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
+              </div>
               <div className='mt-3'>
-                {/* <Button
+                <input
+                  type='password'
+                  className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+                  placeholder='Password'
+                  {...register('password')}
+                />
+                <div className='mt-1 min-h-[1rem] text-sm text-red-600'></div>
+              </div>
+              <div className='mt-3'>
+                <button
                   type='submit'
-                  className='flex  w-full items-center justify-center bg-red-500 py-4 px-2 text-sm uppercase text-white hover:bg-red-600'
-                  isLoading={loginMutation.isLoading}
-                  disabled={loginMutation.isLoading}
+                  className='w-full bg-red-500 py-4 px-2 text-center text-sm uppercase text-white hover:bg-red-600'
                 >
                   Đăng nhập
-                </Button> */}
+                </button>
               </div>
               <div className='mt-8 flex items-center justify-center'>
                 <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
